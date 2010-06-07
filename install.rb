2f0207e105a43a0bf0653e96472f5d446684319f
Vlado Cingel <vladocingel@gmail.com>
2010-06-07 11:47:37 +0200

    install script fix

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 src = "#{Rails.root}/vendor/plugins/mark_it_up/public/mark_it_up"
 dest = "#{Rails.root}/public/mark_it_up"
 puts "* Copying assets to #{dest}"
-File.cp_r(src, dest)
+FileUtils.cp_r(src, dest)
 puts "* Run 'script/generate miu_controller' to generate MarkItUpController and to be able to use default preview parser"
