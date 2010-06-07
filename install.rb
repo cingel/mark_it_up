57919259661c23c138f2a16c9427a04ce96b8b8a
Vlado Cingel <vladocingel@gmail.com>
2010-06-07 11:39:01 +0200

    Install hook added

--- install.rb
+++ install.rb
@@ -1,1 +1,5 @@
-# Install hook code here
+src = "#{Rails.root}/vendor/plugins/mark_it_up/public/mark_it_up"
+dest = "#{Rails.root}/public/mark_it_up"
+puts "* Copying assets to #{dest}"
+File.cp_r(src, dest)
+puts "* Run 'script/generate miu_controller' to generate MarkItUpController and to be able to use default preview parser"
